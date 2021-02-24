10cdf4b9cad96aeab5266adde4e1ddc5c3451d09
connor <connorswannell@me.com>
2021-02-24 12:46:14 +0000

    lazer working

--- game/Assets/Cube Puzzle Game/Assets/Code/laser.cs
+++ game/Assets/Cube Puzzle Game/Assets/Code/laser.cs
@@ -6,23 +6,37 @@ public class laser : MonoBehaviour
 {
     [SerializeField]
     LineRenderer lr;
+    [SerializeField]
+    GameObject point1,point2;
     // Start is called before the first frame update
-    
+    private void Start()
+    {
+        
+    }
     // Update is called once per frame
     void Update()
     {
-        lr.SetPosition(0, transform.position);
+        Vector3 startPos = lr.GetPosition(0);
+        Vector3 endPos = lr.GetPosition(lr.positionCount - 1);
+
+        lr.SetPosition(0, point1.transform.position);
+       
+        
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit))
+       
+        if (Physics.Raycast(point1.transform.position, transform.forward, out hit))
         {
-            if (hit.collider)
-            {
-                lr.SetPosition(1, hit.point);
-            }
+          if (hit.collider)
+          {
+            lr.SetPosition(1, hit.point);
+            print("test");
+          }
         }
         else
         {
-            lr.SetPosition(1, transform.forward * 50);
+                lr.SetPosition(1, point1.transform.forward*5000);
         }
+       
+
     }
 }
